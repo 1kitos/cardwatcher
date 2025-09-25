a2c6aadfff2cebd599da8883d232534e04ed1fbe
1kitos <58308720+1kitos@users.noreply.github.com>
2025-09-26 00:12:07 +0100

    added the rest folder

--- src/main/java/kitos/cardwatcher/controllers/web/GeneralController.java
+++ src/main/java/kitos/cardwatcher/controllers/web/GeneralController.java
@@ -1,4 +1,4 @@
-package kitos.cardwatcher.controllers;
+package kitos.cardwatcher.controllers.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
